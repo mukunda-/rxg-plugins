859d812df3049c72de3496b43a9a5f98ecdd2771
mukunda- <mukunda@mukunda.com>
2014-05-10 18:56:55 -0500

    adding assets

---
+++ store/cookie/assets/compile.bat
@@ -0,0 +1,2 @@
+"E:\programs\stem\steamapps\common\Counter-Strike Global Offensive\bin\studiomdl" -game "E:\programs\stem\steamapps\common\Counter-Strike Global Offensive\csgo" compile.qc
+pause
---
+++ store/cookie/assets/tf2.bat
@@ -0,0 +1,2 @@
+"E:\programs\stem\steamapps\common\Team Fortress 2\bin\studiomdl" -game "E:\programs\stem\steamapps\common\Team Fortress 2\tf" compiletf.qc
+pause
